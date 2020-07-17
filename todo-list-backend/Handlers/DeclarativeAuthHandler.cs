5eb1eeed7583f7f9ce8e8c125e6f67ff524b9e83
Jake Marshall <jake.marshall123@outlook.com>
2020-07-17 16:48:56 +1000

    Refactored authorization handler into abstract class

---
+++ todo-list-backend/Handlers/DeclarativeAuthHandler.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using todo_list_backend.Models.User;
+using todo_list_backend.Services;
+
+namespace todo_list_backend.Handlers
+{
+    public abstract class DeclarativeAuthHandler<TRequestBody, TRequirement> : AuthorizationHandler<TRequirement>
+        where TRequirement : IAuthorizationRequirement
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private IHttpContextAccessor _httpContextAccessor;
+        public DeclarativeAuthHandler(IServiceScopeFactory scopeFactory, IHttpContextAccessor httpContextAccessor)
+        {
+            _scopeFactory = scopeFactory;
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        private async Task<TRequestBody> ReadBody(HttpRequest request)
+        {
+            HttpRequestRewindExtensions.EnableBuffering(request);
+
+            using (StreamReader reader = new StreamReader(request.Body, Encoding.UTF8, leaveOpen: true))
+            {
+                var content = await reader.ReadToEndAsync();
+                request.Body.Position = 0;
+                return JsonConvert.DeserializeObject<TRequestBody>(content);
+            }
+        }
+
+        protected abstract bool ValidateRequestBody(IServiceProvider serviceProvider, UserDto user, TRequestBody body);
+
+        protected async override Task HandleRequirementAsync(AuthorizationHandlerContext context, TRequirement requirement)
+        {
+            using (var scope = _scopeFactory.CreateScope())
+            {
+                var user = (UserDto)this._httpContextAccessor.HttpContext.Items["user"];
+                var resource = await ReadBody(_httpContextAccessor.HttpContext.Request);
+
+                var valid = ValidateRequestBody(scope.ServiceProvider, user, resource);
+
+                if (valid)
+                {
+                    context.Succeed(requirement);
+                }
+                else
+                {
+                    context.Fail();
+                }
+            }
+        }
+    }
+}
--- todo-list-backend/Handlers/NotificationOwnershipHandler.cs
+++ todo-list-backend/Handlers/NotificationOwnershipHandler.cs
@@ -1,61 +1,22 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using todo_list_backend.Models.User;
 using todo_list_backend.Services;
 
 namespace todo_list_backend.Handlers
 {
-    public class NotificationOwnershipHandler : AuthorizationHandler<NotificationsBelongToUserRequirement>
+    public class NotificationOwnershipHandler : DeclarativeAuthHandler<int[], NotificationsBelongToUserRequirement>
     {
-        private readonly IServiceScopeFactory _scopeFactory;
-        private IHttpContextAccessor _httpContextAccessor;
-        public NotificationOwnershipHandler(IServiceScopeFactory scopeFactory, IHttpContextAccessor httpContextAccessor)
+        public NotificationOwnershipHandler(IServiceScopeFactory scopeFactory, IHttpContextAccessor httpContextAccessor) : base(scopeFactory, httpContextAccessor) { }
+
+        protected override bool ValidateRequestBody(IServiceProvider serviceProvider, UserDto user, int[] body)
         {
-            _scopeFactory = scopeFactory;
-            _httpContextAccessor = httpContextAccessor;
-        }
+            var notificationProviderService = serviceProvider.GetRequiredService<INotificationProviderService>();
+            var allNotificationsBelongToUser = notificationProviderService.BelongsToUser(body, user.Id);
 
-        private async Task<T> ReadBody<T>(HttpRequest request)
-        {
-            HttpRequestRewindExtensions.EnableBuffering(request);
-
-            using (StreamReader reader = new StreamReader(request.Body, Encoding.UTF8, leaveOpen: true))
-            {
-                var content = await reader.ReadToEndAsync();
-                request.Body.Position = 0;
-                return JsonConvert.DeserializeObject<T>(content);
-            }
-        }
-
-        protected async override Task HandleRequirementAsync(AuthorizationHandlerContext context, NotificationsBelongToUserRequirement requirement)
-        {
-            using (var scope = _scopeFactory.CreateScope())
-            {
-                var notificationProviderService = scope.ServiceProvider.GetRequiredService<INotificationProviderService>();
-
-                var user = (UserDto)this._httpContextAccessor.HttpContext.Items["user"];
-
-                var resource = await ReadBody<int[]>(_httpContextAccessor.HttpContext.Request);
-
-                var allBelongToUser = notificationProviderService.BelongsToUser(resource, user.Id);
-
-                if (allBelongToUser)
-                {
-                    context.Succeed(requirement);
-                }
-                else
-                {
-                    context.Fail();
-                }
-            }
+            return allNotificationsBelongToUser;
         }
     }
 
